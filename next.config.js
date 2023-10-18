a1bca1dad0bfba12a57832e4c26ac2a5cad0e77f
Emerson Macedo <emerleite@gmail.com>
2023-10-18 20:48:21 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: [
       'pbxt.replicate.delivery',
       'g4yqcv8qdhf169fk.public.blob.vercel-storage.com',
+      'g02deraobxohuuos.public.blob.vercel-storage.com'
     ],
   },
 };
