8f0a215100737406848130a4327246d1895270d8
Emerson Macedo <emerleite@gmail.com>
2023-10-18 20:35:56 -0300

    Update route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -51,7 +51,7 @@ export async function POST(request: NextRequest) {
   const id = nanoid();
   const startTime = performance.now();
 
-  conseole.log(id);
+  console.log(id);
 
   let imageUrl = await replicateClient.generateQrCode({
     url: reqBody.url,
